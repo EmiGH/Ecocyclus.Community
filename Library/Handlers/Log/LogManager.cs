ba3556be0ef3cfba5eefcdbe9a2688d647290696
Emiliano Marroquin <emiliano_m@hotmail.com>
2021-01-07 11:51:22 -0300

    LogManager

---
+++ Library/Handlers/Log/LogManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSI.Library.Handlers
+{
+    internal class LogManager
+    {
+        internal LogManager() { }
+
+        internal void LogError(Int64 idUser, String error)
+        {
+            new Storage.LogManager().LogError(idUser, error);
+        }
+    }
+}
